21fa4c7177e783de6a5bc65d5b9e794110518239
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2023-12-27 00:41:19 +0900

    🔨 jsonを生成するスクリプトを追加

---
+++ tools/update-servers.ts
@@ -0,0 +1,51 @@
+import { writeFileSync } from "fs";
+
+const fetchServers = async <T>(url: string, f: (json: T) => string[]) => {
+  console.log(`[LOG] fetch (${url})`);
+  const res = await fetch(url);
+
+  if (!res.ok) {
+    return;
+  }
+
+  console.log("[LOG] parse");
+  const json = await res.json() as T;
+
+  return f(json);
+};
+
+type MastodonServers = {
+  domain: string;
+}[];
+
+type MisskeyServers = {
+  instancesInfos: {
+    url: string;
+  }[];
+};
+
+(async () => {
+  // Mastodon
+  const mastodon = await fetchServers<MastodonServers>(
+    "https://api.joinmastodon.org/servers?language=&category=&region=&ownership=&registrations=",
+    (json) => json.map(({ domain }) => domain),
+  );
+
+  if (mastodon) {
+    writeFileSync("./public/mastodon.json", JSON.stringify(mastodon));
+  }
+
+  console.log("[DONE] mastodon");
+
+  // Misskey
+  const misskey = await fetchServers<MisskeyServers>(
+    "https://instanceapp.misskey.page/instances.json",
+    (json) => json.instancesInfos.map(({ url }) => url),
+  );
+
+  if (misskey) {
+    writeFileSync("./public/misskey.json", JSON.stringify(misskey));
+  }
+
+  console.log("[DONE] misskey");
+})();
