cf870815de9a0a5334f2e84342efe1c381e3d0c0
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2024-01-06 00:27:38 +0900

    👍 閉じるボタンを追加

--- components/help-dialog.tsx
+++ components/help-dialog.tsx
@@ -1,4 +1,6 @@
+import Link from "next/link";
 import Card from "./card";
+import { RiCloseLine } from "react-icons/ri";
 
 type Section = {
   title: string;
@@ -35,27 +37,35 @@ export default function HelpDialog() {
 
   return (
     <div className="fixed top-0 left-0 z-10 w-screen h-screen flex justify-center items-center bg-black/75">
-      <Card className="space-y-4 text-neutral-800 text-sm">
-        {cardSections}
-        <div>
-          <p className="text-lg font-bold">開発者の方へ</p>
-          <p className="mt-1">
-            URL を作成し、サイトにシェアリンクとして埋め込むことができます。
-          </p>
-          <p className="mt-1">
-            基本的にはシェアしたいテキストを入力し「シェアリンクをコピー」をクリックすれば、URL
-            を作成・コピーできます。
-          </p>
-          <p className="mt-1">
-            詳しいシェアリンクの形式は
-            <a
-              className="underline text-neutral-500 hover:text-blue-500 transition-colors"
-              href="https://github.com/arrow2nd/share/blob/main/README.md#%E4%BD%BF%E3%81%84%E6%96%B9"
-            >
-              GitHub の README
-            </a>
-            に記載しています。
-          </p>
+      <Card className="relative text-neutral-800 text-sm">
+        <Link
+          className="absolute top-0 right-0 text-lg p-4 hover:text-blue-500 transition-colors"
+          href="/"
+        >
+          <RiCloseLine />
+        </Link>
+        <div className="space-y-4">
+          {cardSections}
+          <div>
+            <p className="text-lg font-bold">開発者の方へ</p>
+            <p className="mt-1">
+              URL を作成し、サイトにシェアリンクとして埋め込むことができます。
+            </p>
+            <p className="mt-1">
+              基本的にはシェアしたいテキストを入力し「シェアリンクをコピー」をクリックすれば、URL
+              を作成・コピーできます。
+            </p>
+            <p className="mt-1">
+              詳しいシェアリンクの形式は
+              <a
+                className="underline text-neutral-500 hover:text-blue-500 transition-colors"
+                href="https://github.com/arrow2nd/share/blob/main/README.md#%E4%BD%BF%E3%81%84%E6%96%B9"
+              >
+                GitHub の README
+              </a>
+              に記載しています。
+            </p>
+          </div>
         </div>
       </Card>
     </div>
