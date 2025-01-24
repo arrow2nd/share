fe81c2cf59191dfecd12fa07fbb5d12c5fb08922
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2025-01-24 21:50:51 +0900

    👍 調整

--- components/button.tsx
+++ components/button.tsx
@@ -10,7 +10,7 @@ export default function Button({ secondary = false, ...props }: Props) {
       {...props}
       className={`${
         secondary
-          ? "border border-blue-500 bg-white text-blue-500 hover:border-blue-600 hover:text-blue-600 disabled:border-neutral-200 disabled:text-neutral-200 dark:bg-gray-800"
+          ? "border border-blue-500 bg-white text-blue-500 hover:border-blue-600 hover:text-blue-600 disabled:border-neutral-200 disabled:text-gray-700 disabled:text-neutral-200 dark:bg-gray-800 dark:disabled:border-gray-700"
           : "bg-blue-500 text-white hover:bg-blue-600 disabled:bg-neutral-200"
       } cursor-pointer rounded-full px-5 py-2 text-sm transition-colors sm:text-base`}
     >
